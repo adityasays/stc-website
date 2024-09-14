5772bff26f22a80c970dd3d5be9339caedd56e15
adityasays <aditya.shukla019r@gmail.com>
2024-09-14 18:15:32 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-950 text-white">
     <GoGear className="size-12 animate-spin duration-1000"/>
     <p className="text-xl font-bold py-3">Under Construction</p>
-    <p className="text-gray-300 text-base py-2">good things take time, please wait...</p>
+    <p className="text-gray-300 text-base py-2">good things take time, please wait... hello</p>
     </main>
   );
 }
