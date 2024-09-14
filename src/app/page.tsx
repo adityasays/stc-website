e5d219936dd7f1fc7898bf8e18f5e901e2467c8d
adityasays <aditya.shukla019r@gmail.com>
2024-09-14 18:35:47 +0530

    created stats component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/global/Header";
 import AboveTheFold from "@/components/home/AboveTheFold";
+import Stats from "@/components/home/Stats";
 
 export default function HomePage() {
   return (
     <>
       <Header />
       <AboveTheFold />
+      <Stats />
     </>
   );
 }
