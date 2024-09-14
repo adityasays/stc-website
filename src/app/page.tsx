e63289141092407196e37f3d10d0244823a71402
adityasays <aditya.shukla019r@gmail.com>
2024-09-14 18:23:07 +0530

    header component initialized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import { GoGear } from "react-icons/go";
+import Header from "@/components/global/Header";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-950 text-white">
-    <GoGear className="size-12 animate-spin duration-1000"/>
-    <p className="text-xl font-bold py-3">Under Construction</p>
-    <p className="text-gray-300 text-base py-2">good things take time, please wait... hello</p>
-    </main>
+    <Header />
   );
 }
