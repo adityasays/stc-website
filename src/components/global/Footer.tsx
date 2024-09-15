511b29e36919b795701838ce9770b707f9b0b594
z1shivam <shivamk.services@gmail.com>
2024-09-15 11:59:55 +0530

    initialized footer

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="w-full bg-neutral-900 text-white min-h-96">
+      I am a footer.
+    </footer>
   );
 }
