621839e034ea4941e6db06f801c7d45f906ba90d
z1shivam <shivamk.services@gmail.com>
2024-09-15 11:55:08 +0530

    removed unnecessary classes and resized the desktop abovethefold height

--- src/components/home/AboveTheFold.tsx
+++ src/components/home/AboveTheFold.tsx
@@ -4,7 +4,7 @@ import { Button } from "../ui/button";
 import { FaArrowRight } from "react-icons/fa6";
 
 export default function AboveTheFold() {
-  const words = ["create", "innovate", "develop", "learn"];
+  const words = ["create", "develop", "innovate", "learn"];
   const [currentWord, setCurrentWord] = useState(words[0]);
   const [fade, setFade] = useState(true);
 
@@ -26,21 +26,16 @@ export default function AboveTheFold() {
 
   return (
     <section>
-      <div className="flex min-h-96 w-full select-none flex-col items-center justify-center gap-y-4 text-5xl font-medium sm:text-6xl md:gap-y-2 lg:text-7xl">
+      <div className="flex min-h-96 w-full select-none flex-col items-center justify-center gap-y-4 text-5xl font-medium sm:text-6xl md:min-h-[420px] md:gap-y-2 lg:text-7xl">
         <p className="text-sm text-neutral-500 sm:text-lg">
           SMVDU Tech Community
         </p>
-        <div className="w-fit pb-3 text-center transition-transform duration-700 ease-in-out">
+        <div className="w-fit pb-3 text-center ease-in-out">
           <p>together,</p>
-          <p className="flex items-center justify-center transition-all duration-500">
+          <p className="flex items-center justify-center">
+            <span>we can</span>
             <span
-              className="transition-all duration-500"
-              style={{ marginRight: "10px" }}
-            >
-              we can
-            </span>
-            <span
-              className={`text-yellow-300 transition-all duration-500 ${fade ? "opacity-100" : "opacity-0"}`}
+              className={`ml-3 text-yellow-300 transition-opacity duration-500 ${fade ? "opacity-100" : "opacity-0"}`}
               style={{ display: "inline-block", textAlign: "center" }}
             >
               {currentWord}
@@ -51,7 +46,7 @@ export default function AboveTheFold() {
           className="rounded-full bg-yellow-300 text-black hover:bg-yellow-500"
           size={"default"}
         >
-          Join Now <FaArrowRight className="ml-2"/>
+          Join Now <FaArrowRight className="ml-2" />
         </Button>
       </div>
     </section>
